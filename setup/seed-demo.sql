5e31a4ad8963e131f18dc22dbf0f88b4949c1f19
Michael High <mhigh85@gmail.com>
2019-01-18 17:10:29 -0500

    added cart button to nav

--- setup/seed-demo.sql
+++ setup/seed-demo.sql
@@ -7,7 +7,7 @@ INSERT INTO products (product_name, department_name, price, stock_quantity, phot
 ('OXO Measuring Cup Set','Home & Kitchen',12.00,5,'oxo-measuring-cups.jpg',NOW(),NOW()),
 ('OXO Cutting Board','Home & Kitchen',18.99,8,'oxo-cutting-board.jpg',NOW(),NOW()),
 ('Chirstmas Vacation','Movies, Music, & Games',17.99,25,'christmas-vacation.jpg',NOW(),NOW()),
-('Avengers: Infinity War','Movies, Music, & Games',19.99,30,'avangers-infinity-war.jpg',NOW(),NOW()),
+('Avengers: Infinity War','Movies, Music, & Games',19.99,30,'avengers-infinity-war.jpg',NOW(),NOW()),
 ('Soccer Ball - Atlanta United','Sports & Outdoors',19.99,20,'aufd-soccer-ball.jpg',NOW(),NOW()),
 ('TalorMade Golf Balls','Sports & Outdoors',24.99,5,'talormade-golf-balls.jpg',NOW(),NOW()),
 ('Croft & Barrow 1/4-zip Sweater','Clothing',26.99,2,'cb-qtr-sweater.jpg',NOW(),NOW());
