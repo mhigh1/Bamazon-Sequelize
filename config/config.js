dd7cce6faa2e6da49eb3ed51012d18aa42d38aaf
Michael High <mhigh85@gmail.com>
2019-02-05 14:28:02 -0500

    Added .env

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB,
+    "host": "localhost",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.TEST_DB_USERNAME,
+    "password": process.env.TEST_DB_PASSWORD,
+    "database": process.env.TEST_DB,
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
